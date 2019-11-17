f70282a5b1f48a4f214ff3713eeb6dbba3b2f101
Gutei <anggeraryp@gmail.com>
2019-11-17 11:07:14 +0700

    form register

--- dash/schoreg/forms/schoreg.py
+++ dash/schoreg/forms/schoreg.py
@@ -1,11 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from panel.models import School
+from panel.models import School, SchoolContact, AbstractSchool
+from django.contrib.admin import widgets
 
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class UserForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'style': 'border-bottom:1px solid gray;'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'style': 'border-bottom:1px solid gray;'}))
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput(attrs={'style': 'border-bottom:1px solid gray;'}))
+    password2 = forms.CharField(label='Repeat Password',
+                                widget=forms.PasswordInput(attrs={'style': 'border-bottom:1px solid gray;'}))
 
     class Meta:
         model = User
@@ -15,4 +21,15 @@ class RegisterForm(UserCreationForm):
 class SchoolForm(forms.ModelForm):
     class Meta:
         model = School
-        fields = ["school_number", "name", "level", "address", "logo", "image"]
+        fields = [
+            "school_number", "name", "level", "province", "city",
+            "address", "logo", "image", "level", "type"
+        ]
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = SchoolContact
+        fields = [
+            "phone", "email"
+        ]
--- dash/schoreg/views/register.py
+++ dash/schoreg/views/register.py
@@ -1,16 +1,82 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib import messages
-from schoreg.forms import RegisterForm
+from django.views.generic.edit import CreateView
+from django.db import transaction
+from schoreg.forms import UserForm, SchoolForm, ContactForm
+from panel.models import School, AbstractSchool, SchoolContact
 
 
 # Create your views here.
+class schoreg(CreateView):
+    model = School
+
+    user_form_class = UserForm
+    school_form_class = SchoolForm
+    contact_form_class = ContactForm
+
+    template_name = "schoreg/register.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(School, self).get_context_data(*args, **kwargs)
+    #     return context
+
+    def post(self, request):
+        post_data = request.POST or None
+        user_form = self.user_form_class(post_data, prefix='user')
+        school_form = self.school_form_class(post_data, prefix='school')
+        contact_form = self.contact_form_class(post_data, prefix='contact')
+
+        context = self.get_context_data(
+                                        user_form=user_form,
+                                        school_form=school_form,
+                                        contact_form=contact_form,
+                                        )
+        if school_form.is_valid():
+            try:
+                self.form_save(school_form)
+            except Exception as e:
+                print(e)
+                return render(self.request, 'schoreg/register.html', context)
+
+        if contact_form.is_valid():
+            try:
+                post = self.form_save(contact_form, commit=False)
+                # post.school =
+                post.save()
+            except Exception as e:
+                print(e)
+                return render(self.request, 'schoreg/register.html', context)
+
+        if user_form.is_valid():
+            try:
+                self.form_save(user_form)
+            except Exception as e:
+                print(e)
+                return render(self.request, 'schoreg/register.html', context)
+
+        # if abstractschool_form.is_valid():
+        #     try:
+        #         post = self.form_save(abstractschool_form , commit=False)
+        #         # post.school =
+        #     except Exception as e:
+        #         print(e)
+        #         return render(self.request, 'schoreg/register.html', context)
+
+        return self.render_to_response(context)
+
+    def form_save(self, form):
+        obj = form.save(commit=False)
+        messages.success(self.request, "{} saved successfully".format(obj))
+        return obj
+
+
 def register(request):
     if request.method == "POST":
-        form = RegisterForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Form submission successful')
     else:
-        form = RegisterForm()
+        form = UserForm()
 
     return render(request, "schoreg/register.html", {"form": form})
