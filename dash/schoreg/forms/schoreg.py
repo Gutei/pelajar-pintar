8058a835ea813b0c1b2aa7caa7bfa7d342b6dc0e
Gutei <anggeraryp@gmail.com>
2019-11-20 05:06:47 +0700

    register check schoolnumber duplicat

--- dash/schoreg/forms/schoreg.py
+++ dash/schoreg/forms/schoreg.py
@@ -1,15 +1,18 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserForm(UserCreationForm):
+class UserForm(forms.ModelForm):
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}))
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}))
-    password1 = forms.CharField(label='Password',
-                                widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password'}))
-    password2 = forms.CharField(label='Repeat Password',
-                                widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password'}))
+    password1 = forms.CharField(label='Password', widget=forms.TextInput(attrs={
+        'class': 'form-control', 'type': 'password', 'minlength': '8',
+    }))
+    password2 = forms.CharField(label='Repeat Password', widget=forms.TextInput(attrs={
+        'class': 'form-control', 'type': 'password','minlength': '8',
+    }))
+
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2", ]
--- dash/schoreg/views/register.py
+++ dash/schoreg/views/register.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.contrib import messages
 from django.db import transaction
 from schoreg.forms import UserForm
@@ -23,63 +25,69 @@ def register(request):
     }
     if request.method == "POST":
         form = UserForm(request.POST)
-        if form.is_valid():
-            try:
-                with transaction.atomic():
-                    form.save()
-                    messages.success(request, 'Form submission successful')
-            except Exception as e:
-                messages.error(request, e)
-
-        prov = Province.objects.filter(id=request.POST.get('province')).first()
-        cit = City.objects.filter(id=request.POST.get('city')).first()
-
-        sch = School()
-        sch.school_number = request.POST.get("school_number")
-        sch.name = request.POST.get('name')
-        sch.level = request.POST.get('level')
-        sch.type = request.POST.get('type')
-        sch.address = request.POST.get('address')
-        sch.logo = request.POST.get('logo')
-        sch.image = request.POST.get('image')
-        sch.province = prov
-        sch.city = cit
-
         try:
             with transaction.atomic():
-                sch.save()
-        except Exception as e:
-            messages.error(request, e)
+                if form.is_valid():
+                    try:
+                        form.save()
+                    except Exception as e:
+                        messages.info(request, e)
+                        return redirect(reverse('school-register'))
 
-        reg_1 = sch.pk
+                prov = Province.objects.filter(id=request.POST.get('province')).first()
+                cit = City.objects.filter(id=request.POST.get('city')).first()
 
-        scho = Province.objects.filter(id=reg_1).first()
+                sch = School()
 
-        cont = SchoolContact()
-        cont.school = scho
-        cont.phone = request.POST.get('phone')
-        cont.email = request.POST.get('email')
+                if School.objects.filter(school_number=request.POST.get("school_number")):
+                    messages.success(request, 'Nomor Sekolah Sudah Terdaftar')
+                    return redirect(reverse('school-register'))
+                else:
+                    sch.school_number = request.POST.get("school_number")
 
-        try:
-            with transaction.atomic():
-                cont.save()
-        except Exception as e:
-            messages.error(request, e)
+                sch.name = request.POST.get('name')
+                sch.level = request.POST.get('level')
+                sch.type = request.POST.get('type')
+                sch.address = request.POST.get('address')
+                sch.logo = request.POST.get('logo')
+                sch.image = request.POST.get('image')
+                sch.province = prov
+                sch.city = cit
 
-        reg_2 = form.save().pk
-        usr = User.objects.filter(id=reg_2).first()
+                try:
+                    sch.save()
+                except Exception as e:
+                    messages.error(request, e)
+                    return redirect(reverse('school-register'))
 
-        abst = AbstractSchool()
-        abst.user = usr
-        abst.school = scho
+                reg_1 = sch.id
 
-        try:
-            with transaction.atomic():
-                abst.save()
-        except Exception as e:
-            messages.error(request, e)
+                scho = School.objects.filter(id=reg_1).first()
 
-    else:
-        messages.error(request, 'Form submission Failed')
+                cont = SchoolContact()
+                cont.school = scho
+                cont.phone = request.POST.get('phone')
+                cont.email = request.POST.get('email')
+
+                try:
+                    cont.save()
+                except Exception as e:
+                    messages.error(request, e)
+
+                reg_2 = form.save().pk
+                usr = User.objects.filter(id=reg_2).first()
+
+                abst = AbstractSchool()
+                abst.user = usr
+                abst.school = scho
+
+                try:
+                    abst.save()
+                except Exception as e:
+                    messages.error(request, e)
+
+        except:
+            messages.error(request, 'Form submission Failed')
+            return redirect(reverse('school-register'))
 
     return render(request, 'schoreg/clib/index.html', context)
