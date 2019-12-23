534e0bc4932a4b07b8a1e6db5f3bcf7d0a3e0f7b
Gutei <anggeraryp@gmail.com>
2019-12-24 03:50:24 +0700

    Fixing inject city and prov

--- dash/air/management/commands/inject_city.py
+++ dash/air/management/commands/inject_city.py
@@ -10,16 +10,13 @@ class Command(BaseCommand):
         city = City.objects.all().count()
 
         if city > 0:
-            return None
+            City.objects.all().delete()
 
-        province = Province.objects.all()
-
-        for p in province:
-            province_code = p.code
-            c = requests.get('https://x.rajaapi.com/MeP7c5ne7f0iqtLj3LSaZG9R1GgSziHYVW24b3tVKnSG39B6fZaUw9VLxO/m/wilayah/kabupaten?idpropinsi={}'.format(province_code))
-            # print(c.json().get('data'))
-            # print(dir(c.json()))
-            for d in c.json().get('data'):
-                data = City(province=p, code=d.get('id'), name=d.get('name'))
-                data.save()
-                print(d)
+        c = requests.get("http://jendela.data.kemdikbud.go.id/api/index.php/cwilayah/wilayahKabGet")
+        # print(c.json().get('data'))
+        data = c.json().get('data')
+        # print(dir(c.json()))
+        for d in data:
+            prov = Province.objects.filter(code=d['mst_kode_wilayah']).first()
+            data = City(code=d['kode_wilayah'], name=d['nama'], province=prov)
+            data.save()
